fbff11c25eb33d3b6d251dd69d75fdd93aca136d
zhangkx <zhangkx@jifenn.com>
2020-08-06 18:06:21 +0800

    init

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.less';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="header">
+        <div className="content">hello</div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
